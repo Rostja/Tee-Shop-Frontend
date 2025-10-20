6eeb1fc65da7df44a69c6ce217588609a534b50d
Rostja <rasto.dorcak@gmail.com>
2025-10-20 14:59:50 +0200

    update product-details ts and html, product,service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,6 +16,13 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+   getProduct(theProductId: number): Observable<Product> {
+    //need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     // need to build URL based on category id ... will come back to this later!
