d0cbc36f20ef636971d35a544234ccf21a986ca1
Rostja <rasto.dorcak@gmail.com>
2025-10-22 10:24:14 +0200

    update pagination keyword search

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -22,6 +22,7 @@ export class ProductListComponent implements OnInit {
   thePageNumber: number = 1;
   thePageSize: number = 4;
   theTotalElements: number = 0;
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -48,16 +49,25 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts() {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
+    //if we have different keyword then prevous
+    //then set thePageNumber to 1
+
+    if(this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+    console.log(`keyword = ${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
     //now searching for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                                this.thePageSize,
+                                                theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts() {
-const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    // check if "id" parameter is available
+  const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
   if (hasCategoryId){
     this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
@@ -84,18 +94,21 @@ const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     this.productService.getProductListPaginate(this.thePageNumber - 1,
       this.thePageSize,
-      this.currentCategoryId
-    ).subscribe(data => {
-      this.products = data._embedded.products;
-      this.thePageNumber = data.page.number + 1;
-      this.thePageSize = data.page.size;
-      this.theTotalElements = data.page.totalElements;
-    });
+      this.currentCategoryId).subscribe(this.processResult());
   }
   updatePageSize(pageSize: string){
     this.thePageSize = +pageSize;
     this.thePageNumber = 1;
     this.listProducts();
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
   }
 }
 
