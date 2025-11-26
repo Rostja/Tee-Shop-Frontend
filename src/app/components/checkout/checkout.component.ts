43823277997b3627fcf805769148226adcb07407
Rostja <rasto.dorcak@gmail.com>
2025-11-26 15:58:01 +0100

    populate countries and regions

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TeaShopFormService } from '../../services/tea-shop-form.service';
 import { Country } from '../../common/country';
+import { Region } from '../../common/region';
 
 @Component({
   selector: 'app-checkout',
@@ -21,6 +22,9 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
 
   countries: Country[] = [];
+
+  shippingAddressRegions: Region[] = [];
+  billingAddressRegions: Region[] = [];
 
   constructor(private formBuilder: FormBuilder,
               private teaShopFormService: TeaShopFormService
@@ -117,5 +121,25 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
     )
+  }
+  getRegions(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.teaShopFormService.getRegions(countryCode).subscribe(
+      data => {
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressRegions = data;
+        } else {
+          this.billingAddressRegions = data;
+        }
+        // select first item by default
+        formGroup?.get('region')?.setValue(data[0]);
+      }
+    );
   }
 }
