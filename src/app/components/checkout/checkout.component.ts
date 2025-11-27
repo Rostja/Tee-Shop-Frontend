0c784b74992a135f7d559b4d3932a620a43947b9
Rostja <rasto.dorcak@gmail.com>
2025-11-27 15:36:44 +0100

    define getter methods to access form controls update html template to display error

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -87,6 +87,17 @@ export class CheckoutComponent implements OnInit {
       }
     );
   }
+
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+  
   copyShippingAddressToBillingAddress(event: Event) {
     const checkbox = event.target as HTMLInputElement;
 
