c8c1501f21db9b3039c9a4b302e435c3b73c4ef7
Rostja <rasto.dorcak@gmail.com>
2026-01-14 13:35:47 +0100

    update sending data to backend

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,11 @@ import { Country } from '../../common/country';
 import { Region } from '../../common/region';
 import { TeaShopValidators } from '../../validators/tea-shop-validators';
 import { CartService } from '../../services/cart.service';
+import { CheckoutService } from '../../services/checkout.service';
+import { Router } from '@angular/router';
+import { Order } from '../../common/order';
+import { OrderItem } from '../../common/order-item';
+import { Purchase } from '../../common/purchase';
 
 @Component({
   selector: 'app-checkout',
@@ -30,7 +35,9 @@ export class CheckoutComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private teaShopFormService: TeaShopFormService,
-              private cartService: CartService
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -210,10 +217,73 @@ export class CheckoutComponent implements OnInit {
       return;
     }
 
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
-    console.log("The shipping address is " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
-    console.log("The shipping region is " + this.checkoutFormGroup.get('shippingAddress')?.value.region.name);
+    //set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    //get cart items
+    const cartItems = this.cartService.cartItems;
+
+    //create order items from cart items - long way
+        let orderItems: OrderItem[] = [];
+    for (let i=0; i < cartItems.length; i++) {
+      orderItems[i] = new OrderItem(cartItems[i]);
+      
+    }
+
+    // - short way of doing the same thing
+    /* let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem)); */
+
+    //set up purchase
+    let purchase = new Purchase();
+
+    //populate purchase - custmomer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+    
+    //populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingRegion: Region = JSON.parse(JSON.stringify(purchase.shippingAddress.region));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.region = shippingRegion.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    //populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingRegion: Region = JSON.parse(JSON.stringify(purchase.billingAddress.region));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.region = billingRegion.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    //populate purchase - order and order items
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    //call REST API via the checkout service
+    this.checkoutService.placeOrder(purchase).subscribe({
+      next: response => {
+        alert(`Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`);
+
+        //reset cart
+        this.resetCart();
+      },
+      error: err => {
+        alert(`There was an error: ${err.message}`);
+      }
+    });
+
+  }
+  resetCart() {
+    //reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    //reset the form
+    this.checkoutFormGroup.reset();
+
+    //navigate back to the products page
+    this.router.navigateByUrl("/products");
 
   }
 
