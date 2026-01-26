eb9c53315e6a69e15ac4e82049ddd1a8bce0548c
Rostja <rasto.dorcak@gmail.com>
2026-01-26 11:54:49 +0100

    new stripe packages new typescript declaration file add public key to environments new custom PaymentInfo DTO

---
+++ src/app/common/dist/payment-info.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.PaymentInfo = void 0;
+var PaymentInfo = /** @class */ (function () {
+    function PaymentInfo(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    return PaymentInfo;
+}());
+exports.PaymentInfo = PaymentInfo;
--- src/environments/dist/environment.js
+++ src/environments/dist/environment.js
@@ -3,5 +3,6 @@ exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
     production: false,
-    teaShopApiUrl: 'https://localhost:8443/api'
+    teaShopApiUrl: 'https://localhost:8443/api',
+    stripePublishableKey: 'pk_test_51StXFPLzEfziaDnZzxmLrbPB5NPo5fkeYWtiDwXZK54mWO9cZa7yPW2kKp3v04GBV8DEyVSYmnfplMbZjSVGXk1t00IMybfqEP'
 };
